1b61b0d8c8e247f2a749cba4fa2eefb2637a7699
Kreshnik <kreshnik@sadriu.ch>
2024-05-25 19:34:38 +0200

    v3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hallo Zusammen. Ich will die Nachricht senden, dass ich diese zweite Aufgabe komplett gelöst habe! Version 2');
+  res.send('Hallo Zusammen. Ich will die Nachricht senden, dass ich diese zweite Aufgabe komplett gelöst habe! Version3');
 });
 
 app.listen(PORT, HOST);
